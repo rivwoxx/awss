ca90186a917a4653375ad0ac02b81b362d6d1c2c
rivwox <rivera_augusto@protonmail.com>
2019-12-03 17:39:54 -0600

    Where Is My Mind?

--- Elaine/website/js/config.js
+++ Elaine/website/js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_KStB7zx0F', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '45mk2kect5mjh3g46nrc8o6sai', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_AAC2rV9mk', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3vskvujr9j12mmmtijlb9p5acu', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+        invokeUrl: 'https://2v7sk8ksz4.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
     }
 };
