0eff0e02d3b4ea4efc79ca6f4eae7d9705bff8b5
rivwox <rivera_augusto@protonmail.com>
2019-12-16 20:07:25 -0600

    OC2 added

---
+++ OC2/base.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>RIV</title>
+</head>
+<body>
+
+<?php
+    $db_h ="10.0.2.195";
+    $db_nom ="PRUEBA";
+    $db_usr ="rivx";
+    $db_pss ="rew";
+
+    $conect = mysqli_connect($db_h,$db_usr,$db_pss);
+
+    /* check connection */
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+    $database = mysqli_select_db($conect,$db_nom);
+
+
+?>
+<?php
+
+$result = mysqli_query($conect, "SELECT * FROM pepe");
+
+while($query_data = mysqli_fetch_row($result)) {
+  echo "<table>";
+  echo "<tr>";
+  echo "<td>",$query_data[0], "</td>",
+       "<td>",$query_data[1], "</td>",
+       "<td>",$query_data[2], "</td>";
+  echo "</tr>";
+  echo"</table>";
+}
+?>
+
+</body>
+</html>
+
+ 
